9200650f16a4b56adbc3ac25fa8fa6651546b721
Saif Maamun <sadifmamun@gmail.com>
2024-11-01 15:11:22 +0600

    list operator

---
+++ list.py
@@ -0,0 +1,19 @@
+list1 = ["amir", "john", "dhaka", "BD"]
+print(list1)    # output ['amir', 'john', 'dhaka', 'BD'])
+print(list1[1:3])    # output ['john', 'dhaka'])
+list2 = list([1,2,3,6,5,7,6,8,9])
+print(list2)     #output [1, 2, 3, 6, 5, 7, 8, 9]
+list3 = list((4,6,7,2,1064,66,65))
+print(list3)
+
+list4 = list(("hi", 120, "x70", 5.90))
+print(list4)
+
+list4[1]= 420
+list4[-1]= "akash"
+print(list4)
+print(len(list4))
+list4.append("hasina")
+list2.pop()
+
+print(list2)
