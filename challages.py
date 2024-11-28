2b6f32b594c487a55fdb91c77c12dad34a6f85d7
Saif Maamun <sadifmamun@gmail.com>
2024-11-28 17:11:48 +0600

    chalange 3

--- challages.py
+++ challages.py
@@ -7,10 +7,19 @@
 
 # remove the duplicate element
 
-l1=[int(x) for x in input('enter numbers:').split()]
-l2=[]
+# l1=[int(x) for x in input('enter numbers:').split()]
+# l2=[]
+# for x in l1:
+#     if x not in l2:
+#         l2.append(x)
+# print(l1)
+# print(l2)
+
+# concatenate all integer
+l1=[1,6,8,7,6]
+n=[]
+l=''
 for x in l1:
-    if x not in l2:
-        l2.append(x)
-print(l1)
-print(l2)
+    l+=str(x)
+
+print(int(l))
